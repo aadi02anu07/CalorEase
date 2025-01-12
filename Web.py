7ad7e4de8d211f76abc46eb84f25524a97e7b28b
Aditya Srivastava <53076898+aadi02anu07@users.noreply.github.com>
2025-01-12 19:58:36 +0530

    Update Web.py

--- Web.py
+++ Web.py
@@ -12,11 +12,11 @@ import time
 df1 = pd.read_csv("calories.csv")
 df2 = pd.read_csv("exercise.csv")
 
-image = Image.open("burn.png")
-st.image(image, caption="Calorie Predictor", use_column_width=True)
-
 #Heading
 st.title('Calorie Burnt Predictor')
+
+image = Image.open("burn.png")
+st.image(image, caption="Calorie Predictor", use_column_width=True)
 
 # Take inputs
 user_input_age = st.sidebar.slider('Age', 1, int(df2["Age"].max()), 18)
